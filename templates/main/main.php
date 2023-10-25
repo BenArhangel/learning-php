5189cca7d5f671c5116fc33147930281077179af
BenArhangel <alex.konev.2222@gmail.com>
2023-10-25 16:28:25 +0300

    commit

--- templates/main/main.php
+++ templates/main/main.php
@@ -1,15 +1,16 @@
 <?php include __DIR__ . '/../header.php' ?>
 <div class="cover">
+
     <h1 class="header-one">Статьи</h1>
     
     <p class="paragraph">Здесь вы можете увидеть все статьи</p>
     <div class="inside-cover">
-
+        
             <button id="accc"  class="accordion">Открыть все статьи</button>
             <div class="panel">
                 
                 <?php foreach ($articles as $article): ?>
-
+                    
                         <article class="comment">
                             
                             <div class="article-header">
@@ -21,23 +22,24 @@
                                     <div class="string"></div>
                                     <div class="string"></div>
                                     <div class="string"></div>
-                               </div>
-                               
-                               
+                                </div>
+                                
+                                
+                                
+                                
                             </div>
-                        
-                        
                         <div class="no-content">
                             
                             <p><?= $article->getShortText() ?></p>
+                            <?php if (!empty($user) && $user->isAdmin()){ ?>
+                                <a class="edit" href="/www/articles/<?= $article->getId() ?>/edit">Редактировать статью</a>
+                                <?php }?>
+                            </article>
+                            <?php endforeach; ?>
                         </div>
-                        <?php if (!empty($user) && $user->isAdmin()){ ?>
-                            <a class="edit" href="/www/articles/<?= $article->getId() ?>/edit">Редактировать статью</a>
-                            <?php }?>
-                        </article>
-                        <?php endforeach; ?>
-                    </div>
+                        </div>
+
                     
                 </div>
-            </div>
+          
             <?php include __DIR__ . '/../footer.php' ?>
